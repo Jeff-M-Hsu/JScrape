293da2d7961e3d86aec59caf8ae7b0ed9b5861e7
jhsu42 <jhsu42@uwo.ca>
2019-03-18 00:08:19 -0700

    Changed long string JLabels to JTextPanes

--- JScrape/src/scraper/HelpWindow.java
+++ JScrape/src/scraper/HelpWindow.java
@@ -5,9 +5,11 @@ import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import java.awt.Toolkit;
 
 public class HelpWindow extends JDialog {
 
@@ -25,6 +27,8 @@ public class HelpWindow extends JDialog {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel);
 		contentPanel.setLayout(null);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(HelpWindow.class.getResource("/resources/icon.png")));
+		setTitle("How to use JScrape");
 		
 		JButton okButton = new JButton("OK");
 		okButton.setBounds(width/2-38, height-80, 60, 30);
@@ -42,29 +46,39 @@ public class HelpWindow extends JDialog {
 			}
 		});;
 		
-		JLabel usage = new JLabel("Usage");
-		usage.setBounds(width/2-28, 30, 40, 20);
+		JLabel usage = new JLabel("<html><h3><u>Usage</html>");
+		usage.setBounds(width/2-34, 30, 52, 20);
 		usage.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(usage);
-
-		JLabel usageContent = new JLabel("<html><table><tr><td>Url</td><td>https://example.com/home</td></tr><br><br>"
-				+ "<tr><td>XPath</td><td>div/ul[@class=entry]//li</td></tr><br><br>"
-				+ "<tr><td>Filename</td><td>entries</td></tr><br><br>"
-				+ "<tr><td>Result</td><td>entries.txt containing all list elements in the first unordered list with class value of 'entry' in the first div</td></tr><br><br></table></html>");
-		usageContent.setVerticalAlignment(SwingConstants.TOP);
-		usageContent.setBounds(width/2-208, 60, 400, 120);
+		
+		JTextPane usageContent = new JTextPane();
+		usageContent.setContentType("text/html");
+		usageContent.setText("<html><head><style type=\"text/css\">.text{font-family: Tahoma; font-size:13;}</style></head>"
+				+ "<body><table class=\"text\"><tr><td>Url</td><td>https://www.w3schools.com/xml/xpath_syntax.asp</td></tr><br><br>"
+				+ "<tr><td>XPath</td><td>//div[@id=\"sidebar\"]/nav/h3[last()]/span</td></tr><br><br>"
+				+ "<tr><td>Filename</td><td>link</td></tr><br><br>"
+				+ "<tr><td>Result</td><td>link.txt containing one span element in the last h3 of the nav element in the div with the id \"sidebar\"</td></tr><br><br></table></body></html>");
+		usageContent.setEditable(false);
+		usageContent.setBackground(null);
+		usageContent.setBorder(null);
+		usageContent.setBounds(width/2-194, 60, 392, 120);
 		getContentPane().add(usageContent);
 		
-		JLabel directory = new JLabel("Home Directories");
+		JLabel directory = new JLabel("<html><h3><u>Home Directories</html>");
 		directory.setBounds(width/2-78, 200, 140, 20);
 		directory.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(directory);
 		
-		JLabel directories = new JLabel("<html><table><tr><td>Windows</td><td>&lt;root&gt;\\Users\\&lt;username&gt;</td></tr><br><br>"
+		JTextPane directories = new JTextPane();
+		directories.setContentType("text/html");
+		directories.setText("<html><head><style type=\"text/css\">.text{font-family: Tahoma; font-size:13;}</style></head>"
+				+ "<body><table class=\"text\"><tr><td>Windows</td><td>&lt;root&gt;\\Users\\&lt;username&gt;</td></tr><br><br>"
 				+ "<tr><td>MacOS</td><td>&lt;root&gt;/Users/&lt;username&gt;</td></tr><br><br>"
-				+ "<tr><td>Linux</td><td>/home/username</td></tr></table></html>");
-		directories.setVerticalAlignment(SwingConstants.TOP);
-		directories.setBounds(width/2-208, 230, 400, 120);
+				+ "<tr><td>Linux</td><td>/home/username</td></tr></table></body></html>");
+		directories.setEditable(false);
+		directories.setBackground(null);
+		directories.setBorder(null);
+		directories.setBounds(width/2-194, 230, 392, 120);
 		getContentPane().add(directories);
 	}
 }
